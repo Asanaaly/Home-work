f4aa9e3fad5d7384b4decbc32e62b22245aa130a
Асанаалы Султакеев <asanaalysultakeev@MacBook-Air-Asanaaly.local>
2023-02-09 22:47:59 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,113 @@
+// 1)
+//  let a = 10
+//  a === 10? console.log("верно"): console.log("неверно");
+
+// 2)
+// let a = 50
+// let b = 100
+// a < b ? console.log("a меньше b"): console.log("a больше b");
+
+// 3)
+// let a = -2
+// if(a > 0){
+//     console.log("positive");
+// } else if(a == 0){
+//     console.log("равно");
+// }else{
+//     console.log("negative");
+// }
+
+// 4)
+// let number = 8
+// number % 2 ? console.log("нечетная"): console.log("четная");
+
+// 5)
+// let a = 10
+// let b = 2
+// a % b ? console.log("нечетная"): console.log("четная");
+
+// 6)
+// let s = true 
+// console.log(typeof s);
+
+// let s = 10 
+// console.log(typeof s);
+
+// let s = "Привет"
+// console.log(typeof s);
+
+// 7)
+// let a = 1.1
+// if(a > 1 && a < 9){
+//     console.log("верно");
+// }else{
+//     console.log("неверно");
+// }
+
+
+// 8)
+//  let a = 10
+// if (a === 3 || a === 7) {
+//     a = a + 7
+//  } else {
+//      a = a / 10
+//  }
+//  console.log(a)
+
+
+// 9)
+//  let a = 0
+//  let b = 3
+//  if(a == 0 || a < 0 && b > 3 || b == 3){
+//     console.log(a+b)
+//  }else {
+//     console.log(a-b);
+//  }
+
+
+// 10)
+// let a = 3
+// let b = 7
+// if(a > 4 && a < 10 || b > 7 && b == 7 && b < 17 ){
+//     console.log("верно")
+// }else{
+//     console.log("неверно");
+// }
+
+// 11)
+// let moth = 8
+//     if(moth >= 1 && moth < 3){
+//         console.log("зима");
+//     }else if(moth>=3 && moth < 6){
+//         console.log("весна");
+//     }else if(moth>=6 && moth < 9){
+//         console.log("лето");
+//     }else if(moth>=9 && moth < 12){
+//         console.log("осень");
+//     } else{
+//         console.log("не правильно");
+//     }
+
+
+// 12)
+// let  day = 1
+// if(day >= 1 && day < 11){
+//     console.log("первая");
+// }else if(day >= 11 && day <21){
+//     console.log("вторая");
+// }else if(day >= 21 && day < 31){
+//     console.log("третья");
+// }else{
+//     console.log("больше нету дней");
+// }
+
+// 13)
+// let a='12345'
+// if( a === '1' ){
+//     console.log("yes");
+
+// }else{
+//     console.log("no");
+// }
+// 14)
+// let age = 12
